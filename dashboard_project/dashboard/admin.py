b881a04dae2ccbafd3db401005c75851e9ceaec7
alexlovelady <alexander.s.lovelady@gmail.com>
2024-08-14 21:08:46 -0400

    Remove unnecessary files and update settings

--- dashboard_project/dashboard/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ActivityLog, Analytics
-
-admin.site.register(ActivityLog)
-admin.site.register(Analytics)
--- dashboard_project/dashboard/handlers.py
+++
@@ -1,5 +0,0 @@
-from .models import ActivityLog
-
-def log_activity(user, action):
-    ActivityLog.objects.create(user=user, action=action)
-
