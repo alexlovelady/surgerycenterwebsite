87fbe977c07a4a6c1b81ff9067266e82b8ac5315
alexlovelady <alexander.s.lovelady@gmail.com>
2024-08-24 11:15:27 -0400

    Update footer in HTML files

--- header.js
+++ header.js
@@ -1,8 +1,16 @@
-// JavaScript to dynamically load header and navbar
+// JavaScript to dynamically load header, navbar, and footer
 document.addEventListener('DOMContentLoaded', function() {
+    // Load header
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
+        });
+
+    // Load footer
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
         });
 });
