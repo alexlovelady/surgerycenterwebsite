a6e54eaaaa6d7a8202ae25ff817ca99ca38f1608
alexlovelady <alexander.s.lovelady@gmail.com>
2024-09-04 23:22:59 -0400

    Update contact form layout and remove unnecessary code

--- header.js
+++ header.js
@@ -1,13 +1,10 @@
-// JavaScript to dynamically load header, navbar, and footer
 document.addEventListener('DOMContentLoaded', function() {
-    // Load header
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.body.insertAdjacentHTML('afterbegin', data);
         });
 
-    // Load footer
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
--- testimonials.js
+++ testimonials.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select the container where testimonials should be loaded
     const testimonialsContainer = document.querySelector('.testimonials.container');
 
-    // Load the testimonials from the external HTML file
     fetch('testimonials.html')
         .then(response => response.text())
         .then(data => {
-            testimonialsContainer.innerHTML += data; // Append the loaded testimonials to the container
+            testimonialsContainer.innerHTML += data;
         })
         .catch(error => console.error('Error loading testimonials:', error));
 });
